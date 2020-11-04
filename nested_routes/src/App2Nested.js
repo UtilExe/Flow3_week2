5ef004d2c78233e2a6efd2eba9d7e1a84d7ba2b1
UtilExe <laymasterlolz@gmail.com>
2020-11-04 21:39:21 +0100

    React Router with Nested Routes , code by Lars

--- nested_routes/src/App2Nested.js
+++ nested_routes/src/App2Nested.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles2.css";
+import "./styles/style2.css"
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,44 +9,37 @@ import {
   useParams,
   useRouteMatch
 } from "react-router-dom";
-
-// Since routes are regular React components, they
-// may be rendered anywhere in the app, including in
-// child elements.
-//
-// This helps when it's time to code-split your app
-// into multiple bundles because code-splitting a
-// React Router app is the same as code-splitting
-// any other React app.
-
-export default function NestingExample() {
+ 
+ 
+export default function NestingExample({ info }) {
+ 
   return (
     <Router>
       <div>
         <ul className="header">
           <li>
-            <NavLink to="/">Home</Link>
+            <NavLink exact activeClassName="selected" to="/">Home</NavLink>
           </li>
           <li>
-            <NavLink to="/topics">Topics</NavLink>
+            <NavLink activeClassName="selected" to="/topics">Topics</NavLink>
           </li>
         </ul>
-
+ 
         <hr />
-
+ 
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
           <Route path="/topics">
-            <Topics />
+            <Topics info={info} />
           </Route>
         </Switch>
       </div>
     </Router>
   );
 }
-
+ 
 function Home() {
   return (
     <div>
@@ -54,50 +47,53 @@ function Home() {
     </div>
   );
 }
-
-function Topics() {
-  // The `path` lets us build <Route> paths that are
-  // relative to the parent route, while the `url` lets
-  // us build relative links.
+ 
+function Topics({ info }) {
+  console.log("In Topics", info)
   let { path, url } = useRouteMatch();
-
+  console.log(path, url)
+ 
+  const listItems = info.map(t => (
+    <li key={t.id}>
+      <Link to={`${url}/${t.id}`}>{t.title}</Link>
+    </li>
+  ))
+ 
   return (
     <div>
       <h2>Topics</h2>
       <ul>
-        <li>
-          <Link to={`${url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${url}/props-v-state`}>Props v. State</Link>
-        </li>
+        {listItems}
       </ul>
-
+ 
       <Switch>
         <Route exact path={path}>
           <h3>Please select a topic.</h3>
         </Route>
         <Route path={`${path}/:topicId`}>
-          <Topic />
+          <Topic info={info} />
         </Route>
       </Switch>
     </div>
   );
 }
-
-function Topic() {
-  // The <Route> that rendered this component has a
-  // path of `/topics/:topicId`. The `:topicId` portion
-  // of the URL indicates a placeholder that we can
-  // get from `useParams()`.
+ 
+function Topic(props) {
+ 
   let { topicId } = useParams();
-
+  const topic = props.info.find(element => element.id === topicId);
+ 
   return (
     <div>
-      <h3>{topicId}</h3>
+      { topic && (
+        <div>
+          <h1>{topic.title}</h1>
+          <h3>{topicId}</h3>
+          <p>{topic.info}</p>
+        </div>
+      )
+      }
+      { !topic && <h2>Not Found</h2>}
     </div>
   );
 }
--- nested_routes/src/index.js
+++ nested_routes/src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import reportWebVitals from './reportWebVitals';
 import App from './App2Nested';
-
+ 
+const info = [
+  { id: "rendering", title: "Rendering with React", info: "Add klasdklflasjdfkljjkdsf" },
+  { id: "components", title: "components", info: "Add sajfljasflkdsajflksjak" },
+  { id: "props-v-state", title: "Props v. State", info: "Add some text here" },
+  { id: "react-routing", title: "Routing with React Reouter", info: "Cool text" },
+  { id: "react", title: "Lear React", info: "Cool text about React" },
+]
+ 
+ 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App info={info} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
